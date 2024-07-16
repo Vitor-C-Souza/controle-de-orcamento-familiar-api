a79bd81c675494fef9f29e999ba6f1328f9c22a6
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-07-16 00:00:40 -0300

    Arquivos faltantes não versionados

---
+++ src/main/java/br/com/vitor/controle_de_orcamento_familiar/ControleDeOrcamentoFamiliarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitor.controle_de_orcamento_familiar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeOrcamentoFamiliarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeOrcamentoFamiliarApiApplication.class, args);
+	}
+
+}
